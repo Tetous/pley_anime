9dcc6e8fd896d9210b1f7de7009ffc479c2a674a
root <jw7897@gmail.com>
2020-11-17 01:49:03 +0000

    JS Beautify

--- models/comment.js
+++ models/comment.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
-	user: {
-		id:{
-		type: mongoose.Schema.Types.ObjectID,
-		ref: "User"
-	},
-		username: String
-	},
-	text: String,
-	animeId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Anime"
-	},
+  user: {
+    id: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "User"
+    },
+    username: String
+  },
+  text: String,
+  animeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Anime"
+  },
 });
 
 module.exports = mongoose.model("comment", commentSchema);
--- models/user.js
+++ models/user.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
-	email: {type: String, requried: true, unique: true},
-	username: {type: String, requried: true, unique: true} 
+  email: {
+    type: String,
+    requried: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    requried: true,
+    unique: true
+  }
 })
 
 userSchema.plugin(passportLocalMongoose);
